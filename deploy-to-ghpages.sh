daccb9bb00b098a69f7a667c64ed78e35e70b19b
Josh Duff <me@JoshDuff.com>
2018-10-12 10:05:19 -0500

    A new deploy script mebbe

--- deploy-to-ghpages.sh
+++ deploy-to-ghpages.sh
@@ -1,4 +1,6 @@
-git checkout -b gh-pages
-git add public/bundle.* -f
+git clone -q -b gh-pages https://TehShrike:$GITHUB_API_KEY@github.com/TehShrike/classy-graph deploy
+cp public/* deploy/
+cd deploy
+git add *
 git commit -m 'TravisCI auto-commit'
 git push origin gh-pages
