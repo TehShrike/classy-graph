a7af8e9eb6b7315316fccaa28c5b07a0a4ba1440
Josh Duff <me@JoshDuff.com>
2018-10-12 10:09:00 -0500

    blargh

--- deploy-to-ghpages.sh
+++ deploy-to-ghpages.sh
@@ -1,4 +1,4 @@
-git clone -q -b gh-pages https://TehShrike:$GITHUB_API_KEY@github.com/TehShrike/classy-graph deploy
+git clone -q -b gh-pages https://TehShrike:$GITHUB_API_KEY@github.com/TehShrike/classy-graph.git deploy
 cp public/* deploy/
 cd deploy
 git add *
