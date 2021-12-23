a0700a8649e3e494ee52c56cb96798d8670b3d7e
Josh Duff <me@JoshDuff.com>
2021-12-23 10:16:53 -0600

    Travis is dead, modify the script to be run from dev machine

--- deploy-to-ghpages.sh
+++ deploy-to-ghpages.sh
@@ -1,6 +1,9 @@
-git clone -q -b gh-pages https://TehShrike:$GITHUB_API_KEY@github.com/TehShrike/classy-graph.git deploy
+# git clone -q -b gh-pages https://TehShrike:$GITHUB_API_KEY@github.com/TehShrike/classy-graph.git deploy
+git clone -q -b gh-pages git@github.com:TehShrike/classy-graph.git deploy
 cp public/* deploy/
 cd deploy
 git add *
-git commit -m 'TravisCI auto-commit'
+git commit -m 'script auto-commit'
 git push origin gh-pages
+cd ..
+rm -rf deploy/
