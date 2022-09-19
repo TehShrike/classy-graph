f86630f1d2954c679fcf8d8de364db5d13f1c0f5
Josh Duff <me@JoshDuff.com>
2022-09-19 13:12:13 -0500

    New data

--- big-mac-data/process.js
+++ big-mac-data/process.js
@@ -6,7 +6,7 @@ const isoZones = new Set([ `CAN`, `GBR`, `USA` ])
 const relative = path => require(`path`).join(__dirname, path)
 
 async function main() {
-	const csv = require(`fs`).readFileSync(relative(`./downloaded-2021-12-23.csv`), { encoding: `utf8` })
+	const csv = require(`fs`).readFileSync(relative(`./downloaded-2022-09-19.csv`), { encoding: `utf8` })
 
 	const data = await neatCsv(csv, {
 		mapHeaders: toCamelCase,
